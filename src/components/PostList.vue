a6b496e7f575fd53ff4fccd5d46cfb54db4cac84
Vadym Kononenko <vadymk95@gmail.com>
2022-08-09 18:48:13 +0300

    master/feat/PostList/added animation to list

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -2,12 +2,14 @@
   <div class="post-list">
     <div v-if="posts.length > 0" class="post-list__content">
       <h3 class="post-list__title">List of Posts</h3>
-      <post
-        v-for="post in posts"
-        :post="post"
-        :key="post.id"
-        @remove="$emit('remove', post.id)"
-      />
+      <transition-group name="list" tag="div">
+        <post
+          v-for="post in posts"
+          :post="post"
+          :key="post.id"
+          @remove="$emit('remove', post.id)"
+        />
+      </transition-group>
     </div>
     <h2 v-else class="post-list__empty-title">The list of posts is empty</h2>
   </div>
@@ -38,4 +40,19 @@ export default {
   text-transform: uppercase;
   font-weight: 700;
 }
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(150px);
+}
+
+.list-move {
+  transition: transform 0.8s ease;
+}
+
 </style>
